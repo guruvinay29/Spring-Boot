094ac895d66d55706ee93cd66555c182a5248a91
A RAO <A RAO@ARAO-PC>
2020-03-07 16:35:53 +0530

    SpringBoot|RestWebServices

---
+++ SpringRestController/src/main/java/org/studyeasy/model/AlienRepo.java
@@ -0,0 +1,10 @@
+package org.studyeasy.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel = "aliens",path="aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer>{
+
+}
